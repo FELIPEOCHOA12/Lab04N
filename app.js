cccff9c701276cfdf10449f6f8910e5c3668abe1
Felipe <felipe.ochoa@tecsup.edu.pe>
2025-04-13 22:20:12 -0500

    Initial commit

--- app.js
+++ app.js
@@ -2,7 +2,35 @@ const express = require('express');
 const app = express();
 const port = 9000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// Página principal con botones
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Mi Proyecto Docker</title>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          text-align: center;
+          margin-top: 50px;
+        }
+        button {
+          padding: 15px 30px;
+          margin: 10px;
+          font-size: 16px;
+          cursor: pointer;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Bienvenido a mi proyecto Docker</h1>
+      <button onclick="location.href='/clientes'">Clientes</button>
+      <button onclick="location.href='/productos'">Productos</button>
+    </body>
+    </html>
+  `);
+});
 
 // Rutas adicionales
 app.get('/clientes', (req, res) => {
